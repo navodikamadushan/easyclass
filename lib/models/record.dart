534c3c7781e598516d6ff91f22ba8bf7e30b2429
FlutLab.io <>
2021-12-24 15:34:58 +0000

    Autosave

--- lib/models/record.dart
+++ lib/models/record.dart
@@ -4,18 +4,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Record {
   final String class_name;
   final String subject;
-  final String teacher;
+  final String teacher_id;
   final List<dynamic> timeslot;
   final DocumentReference reference;
 
   Record.fromMap(Map<String, dynamic> map, {this.reference})
       : assert(map['class_name'] != null),
         assert(map['subject'] != null),
-        assert(map['teacher'] != null),
+        assert(map['teacher_id'] != null),
         assert(map['timeslot'] != null),
         class_name = map['class_name'],
         subject = map['subject'],
-        teacher = map['teacher'],
+        teacher_id = map['teacher_id'],
         timeslot = map['timeslot'];
   Record.fromSnapshot(DocumentSnapshot snapshot) : this.fromMap(snapshot.data(), reference: snapshot.reference);
   @override
