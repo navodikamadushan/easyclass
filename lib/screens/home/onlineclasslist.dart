d7eb965eb2e20b5dd7a228064c5e5a4c74b58292
FlutLab.io <>
2021-12-24 16:54:36 +0000

    Autosave

--- lib/screens/home/onlineclasslist.dart
+++ lib/screens/home/onlineclasslist.dart
@@ -53,7 +53,7 @@ class _LessonList extends State<LessonList> {
             children: <Widget>[
               Column(
                 mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: record.timeslot.map((onValue) {
                   return Container(
                     child: Text(
