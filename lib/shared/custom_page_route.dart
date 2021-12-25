8c7543ab2749d62bcf90337d899c1478fc648e6c
FlutLab.io <>
2021-12-25 13:36:04 +0000

    Autosave

--- lib/shared/custom_page_route.dart
+++ lib/shared/custom_page_route.dart
@@ -10,8 +10,11 @@ class CustomPageRoute extends PageRouteBuilder {
         );
 
   @override
-  Widget buildTransitions(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) => ScaleTransition(
-        scale: animation,
+  Widget buildTransitions(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) => SlideTransition(
+        position: Tween<Offset>(
+          begin: Offset(-1, 0),
+          end: Offset.zero,
+        ).animate(animation),
         child: child,
       );
 }
