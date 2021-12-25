f1a66947bdb7c0bb45ec48f53579d2e9275d4f2e
FlutLab.io <>
2021-12-25 04:59:34 +0000

    Autosave

--- lib/shared/splash.dart
+++ lib/shared/splash.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-class Splash extends StatelessWidget {
+class Splash {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
