38a881b7f4ec4954852caf6ef7d40057e2a5c5c0
FlutLab.io <>
2021-12-25 16:41:55 +0000

    Autosave

--- lib/screens/home/onlineclasslist.dart
+++ lib/screens/home/onlineclasslist.dart
@@ -28,7 +28,7 @@ class _LessonList extends State<LessonList> {
 
   Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
     return ListView(
-      itemExtent: 200,
+      //itemExtent: 200,
       scrollDirection: Axis.vertical,
       shrinkWrap: true,
       padding: const EdgeInsets.only(top: 5.0),
