0e926349d331f93a21dfbeb6a7a4bef89e5bfb82
FlutLab.io <>
2021-12-25 17:29:24 +0000

    Autosave

--- lib/screens/home/onlineclasslist.dart
+++ lib/screens/home/onlineclasslist.dart
@@ -12,7 +12,7 @@ class LessonList extends StatefulWidget {
 
 class _LessonList extends State<LessonList> {
   final DatabaseService databaseService = DatabaseService();
-  ScrollController _controller = new ScrollController();
+  //ScrollController _controller = new ScrollController();
   @override
   Widget build(BuildContext context) {
     final userforid = Provider.of<MyUser>(context);
@@ -29,8 +29,8 @@ class _LessonList extends State<LessonList> {
 
   Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
     return ListView(
-      physics: const AlwaysScrollableScrollPhysics(),
-      controller: _controller,
+      //physics: const AlwaysScrollableScrollPhysics(),
+      //controller: _controller,
       scrollDirection: Axis.vertical,
       shrinkWrap: true,
       padding: const EdgeInsets.only(top: 5.0),
