b98b32a6ddeec9fe7f3e1b8034e5a9a33d52c0a7
FlutLab.io <>
2021-12-27 05:37:10 +0000

    Autosave

--- lib/screens/home/onlineclasslist.dart
+++ lib/screens/home/onlineclasslist.dart
@@ -29,14 +29,15 @@ class _LessonList extends State<LessonList> {
 
   Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
     return //SingleChildScrollView(child:
-       ListView//ExpansionPanelList.radio(
-        //physics: const AlwaysScrollableScrollPhysics(),
-        //controller: _controller,
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        padding: const EdgeInsets.only(top: 5.0),
-        children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-        //children: snapshot.map((data) => ExpansionPanelRadio(value: 'Kandy Group', canTapOnHeader: true, headerBuilder: (context, isExpanded) => _buildListItem(context, data))).toList(),
+        ListView(
+      //ExpansionPanelList.radio(
+      //physics: const AlwaysScrollableScrollPhysics(),
+      //controller: _controller,
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      padding: const EdgeInsets.only(top: 5.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+      //children: snapshot.map((data) => ExpansionPanelRadio(value: 'Kandy Group', canTapOnHeader: true, headerBuilder: (context, isExpanded) => _buildListItem(context, data))).toList(),
       //),
     );
   }
