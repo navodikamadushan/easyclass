e2ef68bcd46ca8346509bf90d911aca31233927a
FlutLab.io <>
2022-01-01 13:27:23 +0000

    Autosave

--- lib/screens/home/onlineclasslist.dart
+++ lib/screens/home/onlineclasslist.dart
@@ -71,7 +71,7 @@ class _LessonList extends State<LessonList> {
                   columns: <DataColumn>[
                     DataColumn(
                       label: Container(
-                        width: 30,
+                        //width: 30,
                         child: Text("Date"),
                       ),
                       numeric: false,
