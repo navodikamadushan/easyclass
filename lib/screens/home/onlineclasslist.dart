d5deadd401e91f801c298d1b9f360d776d789756
FlutLab.io <>
2022-01-01 13:24:27 +0000

    Autosave

--- lib/screens/home/onlineclasslist.dart
+++ lib/screens/home/onlineclasslist.dart
@@ -95,7 +95,7 @@ class _LessonList extends State<LessonList> {
                       .map((time) => DataRow(cells: [
                             DataCell(Text(time.split(' ')[0])),
                             DataCell(Container(
-                              width: 20,
+                              width: 50,
                               child: Text(time.split(' ')[1] + ' - ' + time.split(' ')[3]),
                             )),
                             DataCell(Text(time.split(' ')[4])),
