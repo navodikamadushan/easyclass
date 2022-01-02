77e3a3654adccbe9681e97d644eb7ab02c2cd8aa
FlutLab.io <>
2022-01-02 15:24:33 +0000

    Autosave

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -2,7 +2,7 @@
 // Auther Information :- Navodika Karunasingha (eng.navodika@gmail.com)
 
 import 'package:flutter/material.dart';
-import 'package:easyclass/screens/home/teacher/onlineclasspage.dart';
+import 'package:easyclass/screens/home/onlineclasspage.dart';
 import 'package:easyclass/screens/authenticate.dart';
 import 'package:provider/provider.dart';
 import 'package:easyclass/models/user.dart';
