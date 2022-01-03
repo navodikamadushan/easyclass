847e478a6629e1b98093c1de65223af191b5e1cc
FlutLab.io <>
2022-01-03 05:38:30 +0000

    Autosave

--- lib/screens/authlevelwrapper.dart
+++ lib/screens/authlevelwrapper.dart
@@ -16,7 +16,9 @@ class AuthLevelWrapper extends StatelessWidget {
     return StreamBuilder<DocumentSnapshot>(
         stream: FirebaseFirestore.instance.collection('users').doc(userforid.uid).snapshots(),
         builder: (context, snapshot) {
-          return !snapshot.hasData ? Loading() : Scaffold();
+          return !snapshot.hasData ? Loading() : Scaffold(
+            body: Text(snapshot.data['name']);
+          );
         });
   }
 }
