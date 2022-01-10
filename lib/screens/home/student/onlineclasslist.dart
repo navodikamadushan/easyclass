ccd17490f018146646294350417679642a8582bf
FlutLab.io <>
2022-01-10 13:40:30 +0000

    Autosave

--- lib/screens/home/student/onlineclasslist.dart
+++ lib/screens/home/student/onlineclasslist.dart
@@ -20,21 +20,22 @@ class _LessonList extends State<LessonList> {
   @override
   Widget build(BuildContext context) {
     final userforid = Provider.of<MyUser>(context);
-    return StreamBuilder<DocumentSnapshot>( // QuerySnapshot
-      stream: databaseService.onlineclass.doc('71PAS80HCRNv7BAoiKYs').snapshots(),
-      /*databaseService.onlineclass.where('teacher_id', whereIn: [
+    return StreamBuilder<QuerySnapshot>(
+      // QuerySnapshot
+      stream: //databaseService.onlineclass.doc('71PAS80HCRNv7BAoiKYs').snapshots(),
+          databaseService.onlineclass.where('teacher_id', whereIn: [
         userforid.uid
-      ]).snapshots(),*/
+      ]).snapshots(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) return LinearProgressIndicator();
-        print(snapshot.data.toString());
-        return snapshot.data.toString() == "[]"
+        print(snapshot.data.docs.toString());
+        return snapshot.data.docs.toString() == "[]"
             ? Scaffold(
                 body: Center(
                   child: Text('ඔබ කිසිදු පන්තියකට දායක වී නැත.'),
                 ),
               )
-            : _buildList(context, snapshot.data);
+            : _buildList(context, snapshot.data.docs);
       },
     );
   }
