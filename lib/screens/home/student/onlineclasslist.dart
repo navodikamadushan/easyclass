8d52aafd8739ad64b17778ec7e173942d52f374e
FlutLab.io <>
2022-01-10 16:58:23 +0000

    Autosave

--- lib/screens/home/student/onlineclasslist.dart
+++ lib/screens/home/student/onlineclasslist.dart
@@ -27,7 +27,7 @@ class _LessonList extends State<LessonList> {
       ]).snapshots(),*/
       builder: (context, snapshot) {
         if (!snapshot.hasData) return LinearProgressIndicator();
-        print(snapshot.data.docs);
+        print(snapshot.data.docs.documentID);
         return snapshot.data.docs.toString() == "[]"
             ? Scaffold(
                 body: Center(
