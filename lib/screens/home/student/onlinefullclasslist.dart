ef43262941a64751d1b8aa810804431c97e731f3
FlutLab.io <>
2022-01-11 18:40:30 +0000

    Autosave

--- lib/screens/home/student/onlinefullclasslist.dart
+++ lib/screens/home/student/onlinefullclasslist.dart
@@ -24,7 +24,7 @@ class _FullLessonList extends State<FullLessonList> {
   int selected = 0;
   @override
   Widget build(BuildContext context) {
-    print(userInfo['subscribed_class']);
+    print(this.userInfo['subscribed_class']);
     final userforid = Provider.of<MyUser>(context);
     return StreamBuilder<QuerySnapshot>(
       stream: databaseService.onlineclass.snapshots(),
