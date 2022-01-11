06b3676c3a56d2d93505b195adb908239d8163c2
FlutLab.io <>
2022-01-11 13:08:24 +0000

    Autosave

--- lib/screens/home/student/onlineclasspage.dart
+++ lib/screens/home/student/onlineclasspage.dart
@@ -17,6 +17,7 @@ import "package:easyclass/screens/home/student/menu.dart";
 import "package:easyclass/screens/home/student/onlinefullclasspage.dart";
 //import 'package:easyclass/shared/custom_page_route.dart';
 import 'package:top_modal_sheet/top_modal_sheet.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class OnlineClassStudentPage extends StatelessWidget {
   DocumentSnapshot userInfo;
