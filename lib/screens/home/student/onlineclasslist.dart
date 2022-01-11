45232b37d7ac981be6812123d8c59022ce8e9a83
FlutLab.io <>
2022-01-11 04:24:49 +0000

    Autosave

--- lib/screens/home/student/onlineclasslist.dart
+++ lib/screens/home/student/onlineclasslist.dart
@@ -21,9 +21,7 @@ class _LessonList extends State<LessonList> {
   Widget build(BuildContext context) {
     final userforid = Provider.of<MyUser>(context);
     return StreamBuilder<QuerySnapshot>(
-      stream: databaseService.onlineclass.where('teacher_id', whereIn: [
-        userforid.uid
-      ]).snapshots(),
+      stream: databaseService.onlineclass.snapshots(),
       /*databaseService.onlineclass.where('teacher_id', whereIn: [
         userforid.uid
       ]).snapshots(),*/
