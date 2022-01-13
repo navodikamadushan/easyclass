c42d08d8d5e8eec1aa977c613f484aca59d87e6b
FlutLab.io <>
2022-01-13 07:49:58 +0000

    Autosave

--- lib/screens/home/student/zoom/zoom_view_mock.dart
+++ lib/screens/home/student/zoom/zoom_view_mock.dart
@@ -3,5 +3,6 @@ import 'package:flutter/services.dart';
 import "package:easyclass/screens/home/student/zoom/zoom_options.dart";
 import "package:easyclass/screens/home/student/zoom/zoom_platform_view.dart";
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:mockito/mockito.dart';
 
 class ZoomView extends Mock with MockPlatformInterfaceMixin implements ZoomPlatform {}
