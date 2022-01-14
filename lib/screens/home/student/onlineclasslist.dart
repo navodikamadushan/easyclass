a99c490121e9560b0e3f52e9d5901dfa4f15920c
FlutLab.io <>
2022-01-14 12:58:51 +0000

    Autosave

--- lib/screens/home/student/onlineclasslist.dart
+++ lib/screens/home/student/onlineclasslist.dart
@@ -83,12 +83,12 @@ class _LessonList extends State<LessonList> {
             subtitle: Text(record.subject),
             trailing: _buildButton('Join', () async {
               print(record.class_name);
-              /*ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                 content: Text("මෙම විශේෂාංගය තවමත් සංවර්ධනය කර නොමැත."),
-              ));*/
-              Navigator.of(context).push(
+              ));
+              /*Navigator.of(context).push(
                 MaterialPageRoute(builder: (context) => IndexPage()), //'1', '79482849584', '6VrFfY'
-              );
+              );*/
             }),
             onExpansionChanged: (value) {
               //print(expansionTileKey.hashCode.toString());
