135abce19669026953fd931de279f81d0dbafe8e
FlutLab.io <>
2022-01-14 13:56:35 +0000

    Autosave

--- lib/screens/home/student/jitsi/jitsimeeting.dart
+++ lib/screens/home/student/jitsi/jitsimeeting.dart
@@ -1,25 +1,23 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:jitsi_meet/jitsi_meet.dart';
+import 'package:jitsi_meet/jitsi_meeting_listener.dart';
 
-class Meeting extends StatefulWidget {
+class MyApp extends StatefulWidget {
   @override
-  _MeetingState createState() => _MeetingState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MeetingState extends State<Meeting> {
+class _MyAppState extends State<MyApp> {
   final serverText = TextEditingController();
   final roomText = TextEditingController(text: "plugintestroom");
   final subjectText = TextEditingController(text: "My Plugin Test Meeting");
   final nameText = TextEditingController(text: "Plugin Test User");
   final emailText = TextEditingController(text: "fake@email.com");
   final iosAppBarRGBAColor = TextEditingController(text: "#0080FF80"); //transparent blue
-  bool isAudioOnly = true;
-  bool isAudioMuted = true;
-  bool isVideoMuted = true;
+  var isAudioOnly = true;
+  var isAudioMuted = true;
+  var isVideoMuted = true;
 
   @override
   void initState() {
@@ -35,7 +33,6 @@ class _MeetingState extends State<Meeting> {
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -45,144 +42,112 @@ class _MeetingState extends State<Meeting> {
           padding: const EdgeInsets.symmetric(
             horizontal: 16.0,
           ),
-          child: kIsWeb
-              ? Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      width: width * 0.30,
-                      child: meetConfig(),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 24.0,
+                ),
+                TextField(
+                  controller: serverText,
+                  decoration: InputDecoration(border: OutlineInputBorder(), labelText: "Server URL", hintText: "Hint: Leave empty for meet.jitsi.si"),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextField(
+                  controller: roomText,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Room",
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextField(
+                  controller: subjectText,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Subject",
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextField(
+                  controller: nameText,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Display Name",
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextField(
+                  controller: emailText,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Email",
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextField(
+                  controller: iosAppBarRGBAColor,
+                  decoration: InputDecoration(border: OutlineInputBorder(), labelText: "AppBar Color(IOS only)", hintText: "Hint: This HAS to be in HEX RGBA format"),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                CheckboxListTile(
+                  title: Text("Audio Only"),
+                  value: isAudioOnly,
+                  onChanged: _onAudioOnlyChanged,
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                CheckboxListTile(
+                  title: Text("Audio Muted"),
+                  value: isAudioMuted,
+                  onChanged: _onAudioMutedChanged,
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                CheckboxListTile(
+                  title: Text("Video Muted"),
+                  value: isVideoMuted,
+                  onChanged: _onVideoMutedChanged,
+                ),
+                Divider(
+                  height: 48.0,
+                  thickness: 2.0,
+                ),
+                SizedBox(
+                  height: 64.0,
+                  width: double.maxFinite,
+                  child: RaisedButton(
+                    onPressed: () {
+                      _joinMeeting();
+                    },
+                    child: Text(
+                      "Join Meeting",
+                      style: TextStyle(color: Colors.white),
                     ),
-                    Container(
-                        width: width * 0.60,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Card(
-                              color: Colors.white54,
-                              child: SizedBox(
-                                width: width * 0.60 * 0.70,
-                                height: width * 0.60 * 0.70,
-                                child: JitsiMeetConferencing(
-                                  extraJS: [
-                                    // extraJs setup example
-                                    '<script>function echo(){console.log("echo!!!")};</script>',
-                                    '<script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM=" crossorigin="anonymous"></script>'
-                                  ],
-                                ),
-                              )),
-                        ))
-                  ],
-                )
-              : meetConfig(),
+                    color: Colors.blue,
+                  ),
+                ),
+                SizedBox(
+                  height: 48.0,
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
-    );
-  }
-
-  Widget meetConfig() {
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 16.0,
-          ),
-          TextField(
-            controller: serverText,
-            decoration: InputDecoration(border: OutlineInputBorder(), labelText: "Server URL", hintText: "Hint: Leave empty for meet.jitsi.si"),
-          ),
-          SizedBox(
-            height: 14.0,
-          ),
-          TextField(
-            controller: roomText,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Room",
-            ),
-          ),
-          SizedBox(
-            height: 14.0,
-          ),
-          TextField(
-            controller: subjectText,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Subject",
-            ),
-          ),
-          SizedBox(
-            height: 14.0,
-          ),
-          TextField(
-            controller: nameText,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Display Name",
-            ),
-          ),
-          SizedBox(
-            height: 14.0,
-          ),
-          TextField(
-            controller: emailText,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Email",
-            ),
-          ),
-          SizedBox(
-            height: 14.0,
-          ),
-          TextField(
-            controller: iosAppBarRGBAColor,
-            decoration: InputDecoration(border: OutlineInputBorder(), labelText: "AppBar Color(IOS only)", hintText: "Hint: This HAS to be in HEX RGBA format"),
-          ),
-          SizedBox(
-            height: 14.0,
-          ),
-          CheckboxListTile(
-            title: Text("Audio Only"),
-            value: isAudioOnly,
-            onChanged: _onAudioOnlyChanged,
-          ),
-          SizedBox(
-            height: 14.0,
-          ),
-          CheckboxListTile(
-            title: Text("Audio Muted"),
-            value: isAudioMuted,
-            onChanged: _onAudioMutedChanged,
-          ),
-          SizedBox(
-            height: 14.0,
-          ),
-          CheckboxListTile(
-            title: Text("Video Muted"),
-            value: isVideoMuted,
-            onChanged: _onVideoMutedChanged,
-          ),
-          Divider(
-            height: 48.0,
-            thickness: 2.0,
-          ),
-          SizedBox(
-            height: 64.0,
-            width: double.maxFinite,
-            child: ElevatedButton(
-              onPressed: () {
-                _joinMeeting();
-              },
-              child: Text(
-                "Join Meeting",
-                style: TextStyle(color: Colors.white),
-              ),
-              style: ButtonStyle(backgroundColor: MaterialStateColor.resolveWith((states) => Colors.blue)),
-            ),
-          ),
-          SizedBox(
-            height: 48.0,
-          ),
-        ],
       ),
     );
   }
@@ -206,78 +171,43 @@ class _MeetingState extends State<Meeting> {
   }
 
   _joinMeeting() async {
-    String serverUrl = serverText.text.trim().isEmpty ? null : serverText.text;
+    String serverUrl = serverText.text?.trim()?.isEmpty ?? "" ? null : serverText.text;
 
-    // Enable or disable any feature flag here
-    // If feature flag are not provided, default values will be used
-    // Full list of feature flags (and defaults) available in the README
-    Map<FeatureFlagEnum, bool> featureFlags = {
-      FeatureFlagEnum.WELCOME_PAGE_ENABLED: false,
-    };
-    if (!kIsWeb) {
-      // Here is an example, disabling features for each platform
-      if (Platform.isAndroid) {
-        // Disable ConnectionService usage on Android to avoid issues (see README)
-        featureFlags[FeatureFlagEnum.CALL_INTEGRATION_ENABLED] = false;
-      } else if (Platform.isIOS) {
-        // Disable PIP on iOS as it looks weird
-        featureFlags[FeatureFlagEnum.PIP_ENABLED] = false;
-      }
-    }
-    // Define meetings options here
-    var options = JitsiMeetingOptions(room: roomText.text)
-      ..serverURL = serverUrl
-      ..subject = subjectText.text
-      ..userDisplayName = nameText.text
-      ..userEmail = emailText.text
-      ..iosAppBarRGBAColor = iosAppBarRGBAColor.text
-      ..audioOnly = isAudioOnly
-      ..audioMuted = isAudioMuted
-      ..videoMuted = isVideoMuted
-      ..featureFlags.addAll(featureFlags)
-      ..webOptions = {
-        "roomName": roomText.text,
-        "width": "100%",
-        "height": "100%",
-        "enableWelcomePage": false,
-        "chromeExtensionBanner": null,
-        "userInfo": {
-          "displayName": nameText.text
-        }
-      };
+    try {
+      var options = JitsiMeetingOptions()
+        ..room = roomText.text
+        ..serverURL = serverUrl
+        ..subject = subjectText.text
+        ..userDisplayName = nameText.text
+        ..userEmail = emailText.text
+        ..iosAppBarRGBAColor = iosAppBarRGBAColor.text
+        ..audioOnly = isAudioOnly
+        ..audioMuted = isAudioMuted
+        ..videoMuted = isVideoMuted;
 
-    debugPrint("JitsiMeetingOptions: $options");
-    await JitsiMeet.joinMeeting(
-      options,
-      listener: JitsiMeetingListener(
-          onConferenceWillJoin: (message) {
+      debugPrint("JitsiMeetingOptions: $options");
+      await JitsiMeet.joinMeeting(options,
+          listener: JitsiMeetingListener(onConferenceWillJoin: ({message}) {
             debugPrint("${options.room} will join with message: $message");
-          },
-          onConferenceJoined: (message) {
+          }, onConferenceJoined: ({message}) {
             debugPrint("${options.room} joined with message: $message");
-          },
-          onConferenceTerminated: (message) {
+          }, onConferenceTerminated: ({message}) {
             debugPrint("${options.room} terminated with message: $message");
-          },
-          genericListeners: [
-            JitsiGenericListener(
-                eventName: 'readyToClose',
-                callback: (dynamic message) {
-                  debugPrint("readyToClose callback");
-                }),
-          ]),
-    );
+          }));
+    } catch (error) {
+      debugPrint("error: $error");
+    }
   }
 
-  void _onConferenceWillJoin(message) {
+  void _onConferenceWillJoin({message}) {
     debugPrint("_onConferenceWillJoin broadcasted with message: $message");
   }
 
-  void _onConferenceJoined(message) {
+  void _onConferenceJoined({message}) {
     debugPrint("_onConferenceJoined broadcasted with message: $message");
   }
 
-  void _onConferenceTerminated(message) {
+  void _onConferenceTerminated({message}) {
     debugPrint("_onConferenceTerminated broadcasted with message: $message");
   }
 
