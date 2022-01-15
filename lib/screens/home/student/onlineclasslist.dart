1a565a53136687dcd39717285b1918cfc9eacc46
FlutLab.io <>
2022-01-15 08:58:26 +0000

    Autosave

--- lib/screens/home/student/onlineclasslist.dart
+++ lib/screens/home/student/onlineclasslist.dart
@@ -87,7 +87,7 @@ class _LessonList extends State<LessonList> {
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
             subtitle: Text(record.subject),
-            trailing: record.class_name
+            trailing: record.isstart
                 ? _buildButton('Join', () async {
                     print(record.online_class_id);
                     _alertService.joinToExistingClass(context, record.class_name).then((onValue) async {
