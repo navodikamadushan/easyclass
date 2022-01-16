2d755a128a885bb20333fa6d2040efe4b76f5e4f
FlutLab.io <>
2022-01-16 05:20:07 +0000

    Autosave

--- lib/screens/home/teacher/newclass/form_screen.dart
+++ lib/screens/home/teacher/newclass/form_screen.dart
@@ -11,6 +11,7 @@ import 'package:easyclass/models/model.dart';
 import "package:easyclass/services/database.dart"; // import database package
 import "package:easyclass/services/auth.dart"; // import auth package
 import "package:easyclass/models/user.dart"; // import user model for MyUser class
+import "package:easyclass/shared/loading.dart"; // import loading page
 
 class FormScreen extends StatefulWidget {
   @override
@@ -24,6 +25,7 @@ class _FormScreenState extends State<FormScreen> {
   String _subject = "";
   bool isButtonActive = true;
   List<String> _timeSlotlist = [];
+  bool loading = false;
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final DatabaseService _database = DatabaseService(); // instantiating database services
@@ -87,87 +89,89 @@ class _FormScreenState extends State<FormScreen> {
     } else {
       setState(() => isButtonActive = true);
     }
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("පන්ති සැකසුම"),
-        flexibleSpace: Image.asset(
-          "assets/appbar_image.png",
-          fit: BoxFit.cover,
-        ),
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(32),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              _setClassName(),
-              _setSubject(),
-              SizedBox(height: 20),
-              Text(
-                'පන්ති සඳහා ඔබේ කාල පරාසයන් සකසන්න',
-                style: TextStyle(fontSize: 17.0),
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              title: Text("පන්ති සැකසුම"),
+              flexibleSpace: Image.asset(
+                "assets/appbar_image.png",
+                fit: BoxFit.cover,
               ),
-              TimeSlotList(),
-              SizedBox(height: 10),
-              SizedBox(
-                width: 40,
-                height: 40,
-                child: RaisedButton(
-                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-                  onPressed: isButtonActive
-                      ? () {
-                          _addTimeSlotPanel(context);
-                        }
-                      : null,
-                  color: Colors.purple[200],
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
-                  child: Text(
-                    "+",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
+            ),
+            body: SingleChildScrollView(
+              padding: EdgeInsets.all(32),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    _setClassName(),
+                    _setSubject(),
+                    SizedBox(height: 20),
+                    Text(
+                      'පන්ති සඳහා ඔබේ කාල පරාසයන් සකසන්න',
+                      style: TextStyle(fontSize: 17.0),
                     ),
-                  ),
+                    TimeSlotList(),
+                    SizedBox(height: 10),
+                    SizedBox(
+                      width: 40,
+                      height: 40,
+                      child: RaisedButton(
+                        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                        onPressed: isButtonActive
+                            ? () {
+                                _addTimeSlotPanel(context);
+                              }
+                            : null,
+                        color: Colors.purple[200],
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                        child: Text(
+                          "+",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    RaisedButton(
+                      child: Text(
+                        'ඉදිරිපත් කරන්න',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 30),
+                      color: Colors.purple[400],
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+                      onPressed: () async {
+                        print(context.read<ListProvider>().getAllItems(index));
+                        if (!_formKey.currentState.validate()) {
+                          return;
+                        }
+                        // get current user
+                        dynamic user = await _auth.getCurrentUser();
+                        if (user != null) {
+                          print(user.uid.toString());
+                          // add a new online class to database
+                          dynamic result = await _database.addNewOnlineClass(_className, _subject, user.uid.toString(), context.read<ListProvider>().getAllItems(index));
+                          if (result == null) {
+                            _formKey.currentState.save();
+                            // print('');
+                            print('submitted!');
+                          } else {
+                            print('not submitted!');
+                          }
+                        } else {
+                          print('User not logged!');
+                        }
+                      },
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(height: 40),
-              RaisedButton(
-                child: Text(
-                  'ඉදිරිපත් කරන්න',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                ),
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 30),
-                color: Colors.purple[400],
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
-                onPressed: () async {
-                  print(context.read<ListProvider>().getAllItems(index));
-                  if (!_formKey.currentState.validate()) {
-                    return;
-                  }
-                  // get current user
-                  dynamic user = await _auth.getCurrentUser();
-                  if (user != null) {
-                    print(user.uid.toString());
-                    // add a new online class to database
-                    dynamic result = await _database.addNewOnlineClass(_className, _subject, user.uid.toString(), context.read<ListProvider>().getAllItems(index));
-                    if (result == null) {
-                      _formKey.currentState.save();
-                      // print('');
-                      print('submitted!');
-                    } else {
-                      print('not submitted!');
-                    }
-                  } else {
-                    print('User not logged!');
-                  }
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
