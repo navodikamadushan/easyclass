5cbb8d1d2b2aa23a845203c1854cd4de4fb155be
FlutLab.io <>
2022-01-16 14:19:09 +0000

    Autosave

--- lib/services/jitsiservice.dart
+++ lib/services/jitsiservice.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jitsi_meet/jitsi_meet.dart';
 import 'package:jitsi_meet/jitsi_meeting_listener.dart';
+import 'package:jitsi_meet/feature_flag/feature_flag_enum.dart';
+import 'package:jitsi_meet/room_name_constraint.dart';
+import 'package:jitsi_meet/room_name_constraint_type.dart';
 
 class JistiVideoConference {
   testconstructor(String roomName, String subject, String displayname, String useremail) {
