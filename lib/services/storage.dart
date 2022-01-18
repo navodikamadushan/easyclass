96a13b92812d7cf08b5212aa4c8671a549ec7a6d
FlutLab.io <>
2022-01-18 14:08:26 +0000

    Autosave

---
+++ lib/services/storage.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class StorageService {
+  Future uploadImage() async {
+    final _picker = ImagePicker();
+    final _storage = FirebaseStorage.instance;
+    PickedFile image;
+    await Permission.photos.request();
+    var permissionStatus = await Permission.photos.status;
+    if (permissionStatus.isGranted) {
+      image = await _picker.getImage(source: ImageSource.gallery);
+      var file = File(image.path);
+      if (image != null) {
+        var snapshot = await _storage.ref().child("profile_picture/imageName").putFile(file).onComplete;
+        var downloadURL = snapshot.ref.getDownloadURL();
+        print(downloadURL.toString());
+      } else {
+        print("No path received!");
+      }
+    } else {
+      print("Permission is not granted!");
+    }
+  }
+}
