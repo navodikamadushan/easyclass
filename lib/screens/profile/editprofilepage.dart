dedeb00a2a9bd6bd32c4b490e21779cfdd566c87
FlutLab.io <>
2022-01-18 15:23:44 +0000

    Autosave

--- lib/screens/profile/editprofilepage.dart
+++ lib/screens/profile/editprofilepage.dart
@@ -31,7 +31,7 @@ class EditProfilePage extends StatefulWidget {
 
 class _EditProfilePageState extends State<EditProfilePage> {
   //ProUser user = UserPreferences().myUser;
-  String imageURL = null;
+  String imageURL = "HI";
   final _formKey = GlobalKey<FormState>();
   final AuthService _auth = AuthService();
   final StorageService _storage = StorageService();
