985f900301bd1b6244051d10cb6495d07387d522
FlutLab.io <>
2022-01-18 17:00:57 +0000

    Autosave

--- lib/services/storage.dart
+++ lib/services/storage.dart
@@ -15,7 +15,7 @@ class StorageService {
       image = await _picker.getImage(source: ImageSource.gallery);
       print("Hi");
       var file = File(image.path);
-      print("HI2");
+      print(file);
       if (image != null) {
         var snapshot = await _storage.ref().child("profile_picture/imageName").putFile(file);
         var downloadURL = snapshot.ref.getDownloadURL();
