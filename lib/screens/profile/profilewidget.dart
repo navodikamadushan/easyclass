552553e2ab8b8664c92d641f1d22867ac9de4d9a
FlutLab.io <>
2022-01-20 07:06:37 +0000

    Autosave

--- lib/screens/profile/profilewidget.dart
+++ lib/screens/profile/profilewidget.dart
@@ -56,7 +56,7 @@ class ProfileWidget extends StatelessWidget {
           child: IconButton(
             icon: Icon(
               isEdit ? Icons.add_a_photo : Icons.edit,
-              color: Colors.purple,
+              color: Colors.pink,
               size: 20,
             ),
             highlightColor: Colors.pink,
