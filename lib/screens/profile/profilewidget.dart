0243180c49d9c898d31c1243769edb32335513ea
FlutLab.io <>
2022-01-20 06:53:36 +0000

    Autosave

--- lib/screens/profile/profilewidget.dart
+++ lib/screens/profile/profilewidget.dart
@@ -53,10 +53,12 @@ class ProfileWidget extends StatelessWidget {
         child: buildCircle(
           color: color,
           all: 8,
-          child: Icon(
-            isEdit ? Icons.add_a_photo : Icons.edit,
-            color: Colors.white,
-            size: 20,
+          child: IconButton(
+            icon: Icon(
+              isEdit ? Icons.add_a_photo : Icons.edit,
+              color: Colors.white,
+              size: 20,
+            ),
           ),
         ),
       );
