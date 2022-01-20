7183ee54e047b4df5cb957c6f4c19ad7cdd3d389
FlutLab.io <>
2022-01-20 17:04:17 +0000

    Autosave

--- lib/screens/home/teacher/onlineclasslist.dart
+++ lib/screens/home/teacher/onlineclasslist.dart
@@ -81,7 +81,7 @@ class _LessonList extends State<LessonList> {
             subtitle: Text(record.subject),
             trailing: Tooltip(
               message: 'ඔබේ පන්ති විස්තර සංස්කරණය කරන්න.',
-              height: 12,
+              height: 20,
               child: TextButton(
                 style: TextButton.styleFrom(
                   textStyle: const TextStyle(fontSize: 12),
