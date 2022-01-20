88ee6fa77319bd3f8f792049bbd0b21b66cac144
FlutLab.io <>
2022-01-20 15:24:04 +0000

    Autosave

--- lib/screens/home/teacher/newclass/form_screen.dart
+++ lib/screens/home/teacher/newclass/form_screen.dart
@@ -161,7 +161,7 @@ class _FormScreenState extends State<FormScreen> {
                           print(user.uid.toString());
                           // add a new online class to database
                           setState(() => loading = true);
-                          dynamic result = await _database.addNewOnlineClass(_className, _subject, user.uid.toString(), userInfo['name'], context.read<ListProvider>().getAllItems(index));
+                          dynamic result = await _database.addNewOnlineClass(_className, _subject, user.uid.toString(), widget.userInfo['name'], context.read<ListProvider>().getAllItems(index));
                           if (result == null) {
                             setState(() => loading = false);
                             //_formKey.currentState.save();
