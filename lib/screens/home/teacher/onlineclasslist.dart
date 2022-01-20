6a832c4e17b28bb87d2a20f211a9da6e0ca0cac5
FlutLab.io <>
2022-01-20 16:52:25 +0000

    Autosave

--- lib/screens/home/teacher/onlineclasslist.dart
+++ lib/screens/home/teacher/onlineclasslist.dart
@@ -86,7 +86,7 @@ class _LessonList extends State<LessonList> {
               onPressed: () {
                 print("Edit!");
               },
-              child: const Text('Edit'),
+              child: const Text('සංස්කරණය'),
             ),
             onExpansionChanged: (value) {
               //print(expansionTileKey.hashCode.toString());
