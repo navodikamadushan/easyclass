d75d75bf65823ff1561a7053a91254aaf673803f
FlutLab.io <>
2022-01-20 16:45:31 +0000

    Autosave

--- lib/screens/home/student/onlinefullclasslist.dart
+++ lib/screens/home/student/onlinefullclasslist.dart
@@ -84,7 +84,7 @@ class _FullLessonList extends State<FullLessonList> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text("විෂය: " + record.subject),
-                Text("ගුරුවරයා: Navodika Madushan"),
+                Text("ගුරුවරයා: ${record.teachername}"),
               ],
             ),
             enabled: widget.userInfo['subscribed_class'].contains(record.online_class_id) ? false : true,
