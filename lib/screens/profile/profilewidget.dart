a9bd24d949acceb31dba25dcdb63e59e0c73f196
FlutLab.io <>
2022-01-20 08:29:40 +0000

    Autosave

--- lib/screens/profile/profilewidget.dart
+++ lib/screens/profile/profilewidget.dart
@@ -61,7 +61,7 @@ class ProfileWidget extends StatelessWidget {
             ),
             highlightColor: Colors.pink,
             onPressed: () {
-              !isEdit ? this.onClicked : print("Edit pro");
+              !isEdit ? onClicked : print("Edit pro");
               //onClicked;
             },
           ),
