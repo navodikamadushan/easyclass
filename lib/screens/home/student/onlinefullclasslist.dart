db7ef84bc52fdaa9e9fb5b1e70b94056262b7af6
FlutLab.io <>
2022-01-20 13:51:24 +0000

    Autosave

--- lib/screens/home/student/onlinefullclasslist.dart
+++ lib/screens/home/student/onlinefullclasslist.dart
@@ -82,7 +82,7 @@ class _FullLessonList extends State<FullLessonList> {
             ),
             subtitle: Column(
               children: [
-                Text(record.subject),
+                "hi" + Text(record.subject),
                 Text(record.subject),
               ],
             ),
