41db1a0e8e56018e64316a5adf896885c9b747c7
FlutLab.io <>
2022-01-21 13:13:42 +0000

    Autosave

--- lib/screens/home/teacher/onlineclasspage.dart
+++ lib/screens/home/teacher/onlineclasspage.dart
@@ -30,7 +30,7 @@ class OnlineClassTeacherPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('පන්ති'),
+        title: Text('මගේ පන්ති'),
         centerTitle: true,
         actions: <Widget>[
           ProfileImageWidget(
