6d41878d9252dddfc00a35505f090057e4a3ee2f
FlutLab.io <>
2022-01-21 11:09:19 +0000

    Autosave

--- lib/screens/home/teacher/onlineclasslist.dart
+++ lib/screens/home/teacher/onlineclasslist.dart
@@ -77,12 +77,12 @@ class _LessonList extends State<LessonList> {
             title: Row(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                const SizedBox(width: 20.0, height: 100.0),
+                const SizedBox(width: 20.0, height: 50.0),
                 const Text(
                   'Be',
-                  style: TextStyle(fontSize: 43.0),
+                  style: TextStyle(fontSize: 15.0),
                 ),
-                const SizedBox(width: 20.0, height: 100.0),
+                const SizedBox(width: 20.0, height: 15.0),
                 DefaultTextStyle(
                   style: const TextStyle(
                     fontSize: 40.0,
