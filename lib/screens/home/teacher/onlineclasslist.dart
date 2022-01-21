8210f83909e1f283a98d90c731d333fccc7b93c5
FlutLab.io <>
2022-01-21 05:14:30 +0000

    Autosave

--- lib/screens/home/teacher/onlineclasslist.dart
+++ lib/screens/home/teacher/onlineclasslist.dart
@@ -110,8 +110,8 @@ class _LessonList extends State<LessonList> {
                     child: Text(
                       'සිසුන් සංඛ්‍යාව: ${record.no_student.toString()}',
                       style: TextStyle(
-                        fontSize: 20.0,
-                      ),
+                          // fontSize: 20.0,
+                          ),
                     ),
                   ),
                 ],
